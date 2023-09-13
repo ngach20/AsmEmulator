2ef122c0893b60e5dd14258a25b7ff4d7a736d8e
ngach20 <ngach20@freeuni.edu.ge>
2023-09-13 12:48:26 +0400

    refractoring

--- build/CMakeFiles/interpreter.dir/DependInfo.cmake
+++ build/CMakeFiles/interpreter.dir/DependInfo.cmake
@@ -1,22 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/nick/AsmEmulator/AsmEmulator/instruction_set.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/instruction_set.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/main.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/main.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/reader.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/reader.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/translator.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/translator.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/vm/cpu.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/vm/cpu.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/vm/memory.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/vm/memory.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/vm/screen.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/vm/screen.cpp.o"
-  "/home/nick/AsmEmulator/AsmEmulator/vm/vm.cpp" "/home/nick/AsmEmulator/build/CMakeFiles/interpreter.dir/AsmEmulator/vm/vm.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nick/AsmEmulator/AsmEmulator/instruction_set.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/instruction_set.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/instruction_set.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/main.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/main.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/main.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/reader.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/reader.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/reader.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/translator.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/translator.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/translator.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/vm/cpu.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/vm/cpu.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/vm/cpu.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/vm/memory.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/vm/memory.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/vm/memory.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/vm/screen.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/vm/screen.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/vm/screen.cpp.o.d"
+  "/home/nick/AsmEmulator/AsmEmulator/vm/vm.cpp" "CMakeFiles/interpreter.dir/AsmEmulator/vm/vm.cpp.o" "gcc" "CMakeFiles/interpreter.dir/AsmEmulator/vm/vm.cpp.o.d"
   )
 
 # Targets to which this target links.
